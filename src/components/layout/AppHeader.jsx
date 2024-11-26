b19f9bac1d83f4ac926f57c7343c95c91e0c1104
Lev <leevkrasnov@gmail.com>
2024-11-26 22:07:43 +0500

    feat: AppHeader: add button and select list

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,14 +1,61 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+import { useState, useEffect } from 'react';
 
 const headerStyle = {
+  width: '100%',
   textAlign: 'center',
-  color: '#fff',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
+  padding: '1 rem',
+  display: 'flex',
+  background: 'white',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 };
 
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+  const [select, setSelect] = useState(false);
+  const { crypto } = useCrypto();
+
+  useEffect(() => {
+    const keypress = (event) => {
+      if (event.key === '/') {
+        setSelect((prev) => !prev);
+      }
+    };
+    document.addEventListener('keypress', keypress);
+    return () => document.removeEventListener('keypress', keypress);
+  }, []);
+
+  function handleSelect(value) {}
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{
+          width: '250px',
+        }}
+        open={select}
+        onSelect={handleSelect}
+        onClick={() => setSelect((prev) => !prev)}
+        value="press / to open"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              src={option.data.icon}
+              alt={option.data.label}
+              style={{ width: 20 }}
+            />{' '}
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add Asset</Button>
+    </Layout.Header>
+  );
 }
--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { fakeFetchCrypto, fakeFetchAssets } from '../api';
 import { percentDifference } from '../utils';
 
@@ -44,3 +44,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext);
+}
