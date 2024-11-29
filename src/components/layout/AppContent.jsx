cefc8e8d4663fb19a756d2b59a28cd43b6aa704d
Lev <leevkrasnov@gmail.com>
2024-11-29 19:59:37 +0500

    feat: add "PortfolioChart"

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,13 +1,36 @@
-import { Layout } from 'antd';
+import { Layout, Typography } from 'antd';
+import { useCrypto } from '../../context/crypto-context';
+import AssetsTable from './AssetsTable';
+import PortfolioChart from './PortfolioChart';
 
 const contentStyle = {
   textAlign: 'center',
   minHeight: 'calc(100vh - 60px)',
   lineHeight: '120px',
   color: '#fff',
-  backgroundColor: '#0958d9',
+  backgroundColor: '#001529',
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, coin) => {
+    acc[coin.id] = coin.price;
+    return acc;
+  }, {});
+
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: 'left', color: '#fff' }}>
+        Portfolio:{' '}
+        {assets
+          .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+          .reduce((acc, value) => (acc += value), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+      <PortfolioChart />
+      <AssetsTable />
+    </Layout.Content>
+  );
 }
